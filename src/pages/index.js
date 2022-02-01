f5cf4caf5a6a2ec0e38a0e39a2f4ba40d0738cfe
Binh Nguyen <rbtnguyen@gmail.com>
2022-02-01 00:05:13 -0800

    first few tasks done

---
+++ src/pages/index.js
@@ -0,0 +1,31 @@
+import * as React from "react";
+import { Link, useStaticQuery, graphql } from 'gatsby';
+import { Seo } from '../components/seo.js';
+
+export default function IndexPage() {
+    const data = useStaticQuery(graphql`
+        query GetSiteTitle {
+            site {
+                siteMetadata {
+                    title
+                }
+            }
+        }
+    `);
+
+    const meta = data?.site?.siteMetadata ?? {};
+
+    return (
+        <>
+            <Seo />
+            <header>
+                <Link to="/">{meta.title}</Link>
+            </header>
+            <main>
+                <h1>Hello Frontend Masters!</h1>
+                <Link to="/about">About this site</Link>
+            </main>
+        </>
+
+    );
+}
